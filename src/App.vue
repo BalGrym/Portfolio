a7d5ac96df69665a05ebbe03ae4fce0c99fdfb4a
BalGrym <kevin_ber_69@hotmail.fr>
2024-08-08 09:49:15 +0200

    nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<template>
+  <the-navigation></the-navigation>
+  <bento-wrapper></bento-wrapper>
+</template>
 
-<template></template>
+<script>
+import TheNavigation from "./components/nav/TheNavigation.vue";
+import BentoWrapper from "./components/bento/BentoWrapper.vue";
+
+export default {
+  components: {
+    TheNavigation,
+    BentoWrapper,
+  },
+};
+</script>
 
 <style scoped></style>
---
+++ src/components/nav/TheNavigation.vue
@@ -0,0 +1,62 @@
+<template>
+  <header>
+    <img src="../../assets/logo/logo.png" alt="icon" class="logo" />
+    <nav>
+      <ul>
+        <li><a href="#">Acceuil</a></li>
+        <li><a href="#">À propos</a></li>
+        <li><a href="#">Projets</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <ul>
+      <li><i class="fa-brands fa-linkedin fa-xl"></i></li>
+      <li><i class="fa-brands fa-github-square fa-xl"></i></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+ul {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  list-style-type: none;
+  height: 100%;
+  padding: 0;
+}
+
+.logo {
+  height: 80px;
+  /* width: 80px; */
+}
+
+nav {
+  border: 1px solid #3d3d3d;
+  border-radius: 80px;
+  height: 60px;
+}
+
+nav ul {
+  padding: 6px;
+}
+
+nav li {
+  padding: 10px 50px;
+}
+
+nav li:active {
+  background-color: grey;
+  border-radius: 80px;
+}
+</style>
